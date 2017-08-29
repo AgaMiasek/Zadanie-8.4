249b381289c457c13f55d5b5c22ca1f10cf06004
pmiasek <pmiasek@ce5.pl>
2017-08-29 13:05:42 +0200

    dodanie plikow do projektu

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+//alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+/*to jest komentarz blokowy, 
+bardzo długi
+komentarz blokowy*/
+
+// console.log('The commented will not be called');
